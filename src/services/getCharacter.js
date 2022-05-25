931e6cde5191adf9b98d5da155a2304832071899
Jessie <miss_sc_jessi@hotmail.com>
2022-05-25 17:45:34 -0300

    no filters

--- src/services/getCharacter.js
+++ src/services/getCharacter.js
@@ -4,11 +4,8 @@ const getAllCharacter = async () => {
   const { data } = await axios.get(
     "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
   );
-  const dc = data.filter(
-    ({ biography: { publisher } }) =>
-      publisher === "DC Comics" || publisher === "Superman Prime One-Million"
-  );
-  return dc;
+  console.log(data);
+  return data;
 };
 
 export default getAllCharacter;
